a54bade0d0608d463998c3f071580846f47e993e
Uniquecon <akinwunmiolusegun277@gmail.com>
2023-08-07 19:21:06 -0700

    updated files

--- acc_page/page1/page1.js
+++ acc_page/page1/page1.js
@@ -5,3 +5,38 @@ document.getElementById("serIconBtn").addEventListener("click", ()=> {
 document.getElementById("navBackBtn").addEventListener("click", ()=> {
     document.getElementById("searchDiv").classList.remove("openSearchDiv")
 })
+
+let textArray = []
+document.getElementById("navSearchBtn").addEventListener("click", ()=> {
+    let searchText = document.getElementById("navSearchField").value
+    let textObj = {
+        id: textArray.length,
+        text: searchText
+    }
+    textArray.unshift(textObj)
+    shiftText(textArray)
+    localStorage.setItem("textInput", JSON.stringify(textArray))
+    searchText = ""
+})
+
+function shiftText(textBox) {
+    let textDiv = ``
+    for(let x = 0; x < textBox.length; x++) {
+        textDiv += `
+            <div class="searchedMovieDiv">
+                <div class="moveDescDiv">
+                    <img src="images/laptop_main.jpg" alt="" class="movieImage">
+                    <p class="movieName">${textBox[x].text}</p>
+                </div>
+                <p><i class="fa-regular fa-circle-play playBtn"></i></p>
+            </div>
+        `
+    }
+    document.getElementById("mainEntryDiv").innerHTML = textDiv
+}
+
+let textArrayFromLocal = JSON.parse(localStorage.getItem("textInput"))
+if(textArrayFromLocal) {
+    let textArray = textArrayFromLocal
+    shiftText(textArray)
+}
